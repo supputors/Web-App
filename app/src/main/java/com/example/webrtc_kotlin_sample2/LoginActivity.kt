9d77943d733abf5de33020f9d40cb643a6150581
chaeyeon <youcy0426@gmail.com>
2021-07-26 18:01:40 +0900

    validation

--- app/src/main/java/com/example/webrtc_kotlin_sample2/LoginActivity.kt
+++ app/src/main/java/com/example/webrtc_kotlin_sample2/LoginActivity.kt
@@ -7,7 +7,9 @@ package com.example.webrtc_kotlin_sample2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,15 +17,28 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        /* 로그인 버튼 클릭시 처리*/
         btn_login.setOnClickListener {
-            /* 로그인 조건 */
+            val ps: Pattern = Pattern.compile("^[a-zA-Z0-9\\u318D\\u119E\\u11A2\\u2022\\u2025a\\u00B7\\uFE55]+$")
 
-            /* 로그인 이후 화면전환 */
-            val HomeIntent = Intent(this, HomeActivity::class.java)
-            startActivity(HomeIntent)
+            /* 유효성 검사 및 로그인 */
+            if(user_id.text.toString() == ""){
+                Toast.makeText(this, "아이디에 공백이 존재합니다.", Toast.LENGTH_SHORT).show()
+            }else if(user_pw.text.toString() == ""){
+                Toast.makeText(this, "비밀번호에 공백이 존재합니다.", Toast.LENGTH_SHORT).show()
+            }else if(!ps.matcher(user_id.text.toString()).matches()){
+                Toast.makeText(this, "아이디에는 특수문자가 존재할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else{
+                /* 로그인 이후 화면전환 */
+                val HomeIntent = Intent(this, HomeActivity::class.java)
+                startActivity(HomeIntent)
+                Toast.makeText(this, "로그인이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+
         }
+
+        /* 회원가입 버튼 클릭시 처리 */
         btn_signup.setOnClickListener {
-            /* 회원가입 화면 전환 */
             val SignupIntent = Intent(this, SignupActivity::class.java)
             startActivity(SignupIntent)
         }
--- app/src/main/java/com/example/webrtc_kotlin_sample2/SignupActivity.kt
+++ app/src/main/java/com/example/webrtc_kotlin_sample2/SignupActivity.kt
@@ -4,13 +4,47 @@
 
 package com.example.webrtc_kotlin_sample2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        /* 회원가입 버튼 클릭시 처리*/
+        btn_signup.setOnClickListener {
+            var id : String? = signup_id.text.toString()
+            var nick : String? = signup_nick.text.toString()
+            var pw : String? = signup_pw.text.toString()
+            var name : String? = signup_name.text.toString()
+            var birth : String? = signup_birth.text.toString()
+            var phone : String? = signup_phone.text.toString()
+
+            val ps: Pattern = Pattern.compile("^[a-zA-Z0-9\\u318D\\u119E\\u11A2\\u2022\\u2025a\\u00B7\\uFE55]+$")
+
+            if(id == "" || nick == "" || pw == "" || name == "" || birth == "" || phone == ""){
+                Toast.makeText(this, "회원 정보에 공백이 존재합니다.", Toast.LENGTH_SHORT).show()
+            }else if(!ps.matcher(id).matches()){
+                Toast.makeText(this, "아이디에는 특수문자가 존재할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else if(!ps.matcher(nick).matches()){
+                Toast.makeText(this, "닉네임에는 특수문자가 존재할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else if(!ps.matcher(name).matches()){
+                Toast.makeText(this, "이름에는 특수문자가 존재할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else if(!ps.matcher(birth).matches()){
+                Toast.makeText(this, "생일에는 특수문자가 존재할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else if(!ps.matcher(phone).matches()){
+                Toast.makeText(this, "핸드폰번호에는 특수문자가 존재할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else {
+                val loginIntent = Intent(this, LoginActivity::class.java)
+                Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
